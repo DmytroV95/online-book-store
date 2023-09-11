631f5ecb293cb9b02064a871604ef9ff66b9acfb
Dmytro Varukha <dmytro.varukha@gmail.com>
2023-09-11 18:26:48 +0300

    added user and role entity

---
+++ src/main/java/com/varukha/onlinebookstore/model/Role.java
@@ -0,0 +1,31 @@
+package com.varukha.onlinebookstore.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Data
+@Table(name = "role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, unique = true)
+    private RoleName name;
+
+    enum RoleName {
+        USER, ADMIN
+    }
+}
